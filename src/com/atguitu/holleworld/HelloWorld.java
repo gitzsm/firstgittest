d5d1d00900bbca01c5e5e08a35c505b2e5ae2fb7
gitzsm <1204362811@qq.com>
2018-05-16 19:50:46 +0800

    weiwei bangbagnda

--- src/com/atguitu/holleworld/HelloWorld.java
+++ src/com/atguitu/holleworld/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("123");
 		System.out.println("张总工真牛");
-		System.out.println("weiweizongzhenniu");
+		System.out.println("weiweizongzhenniu bangbangda");
 	}
 }
